17fd5626bb8fc6c293cbd06de992d95637202efa
Ivan Eremin <ereminspb@gmail.com>
2024-07-23 15:50:57 +0300

    Update main.py
    Replacing with double quotes:
    https://yandex.cloud/ru/docs/functions/lang/python/handler

--- main.py
+++ main.py
@@ -1,52 +1,51 @@
-#  Copyright (c) ChernV (@otter18), 2021.
-
+#for update Ivan Eremin 2024 like this docs https://yandex.cloud/ru/docs/functions/lang/python/handler
 import os
 import random
 import telebot
 
-bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
+bot = telebot.TeleBot(os.environ.get("BOT_TOKEN"))
 
 
 # ---------------- dialog params ----------------
 dialog = {
-    'hello': {
-        'in': ['/hello', 'привет', 'hello', 'hi', 'privet', 'hey'],
-        'out': ['Приветствую', 'Здравствуйте', 'Привет!']
+    "hello": {
+        "in": ["/hello", "привет", "hello", "hi", "privet", "hey"],
+        "out": ["Приветствую", "Здравствуйте", "Привет!"]
     },
-    'how r u': {
-        'in': ['/howru', 'как дела', 'как ты', 'how are you', 'дела', 'how is it going'],
-        'out': ['Хорошо', 'Отлично', 'Good. And how are u?']
+    "how r u": {
+        "in": ["/howru", "как дела", "как ты", "how are you", "дела", "how is it going"],
+        "out": ["Хорошо", "Отлично", "Good. And how are u?"]
     },
-    'name': {
-        'in': ['/name', 'зовут', 'name', 'имя'],
-        'out': [
-            'Я telegram-template-bot',
-            'Я бот шаблон, но ты можешь звать меня в свой проект',
-            'Это секрет. Используй команду /help, чтобы узнать'
+    "name": {
+        "in": ["/name", "зовут", "name", "имя"],
+        "out": [
+            "Я telegram-template-bot",
+            "Я бот шаблон, но ты можешь звать меня в свой проект",
+            "Это секрет. Используй команду /help, чтобы узнать"
         ]
     }
 }
 
 
 # --------------------- bot ---------------------
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=["help", "start"])
 def say_welcome(message):
     bot.send_message(message.chat.id,
-                     'Hi, there! I am hosted by Yandex.Cloud Functions.\n'
-                     'For more info click [here](https://github.com/otter18/serverless-tg-bot)',
-                     parse_mode='markdown')
+                     "Hi, there! I am hosted by Yandex.Cloud Functions.\n"
+                     "For more info click [here](https://github.com/otter18/serverless-tg-bot)",
+                     parse_mode="markdown")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo(message):
     for t, resp in dialog.items():
-        if sum([e in message.text.lower() for e in resp['in']]):
-            bot.send_message(message.chat.id, random.choice(resp['out']))
+        if sum([e in message.text.lower() for e in resp["in"]]):
+            bot.send_message(message.chat.id, random.choice(resp["out"]))
             return
 
-    bot.send_message(message.chat.id, 'Seems wrong. Use /help to discover more')
+    bot.send_message(message.chat.id, "Seems wrong. Use /help to discover more")
 
 
 # ---------------- local testing ----------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.infinity_polling()
